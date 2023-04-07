5ff14729a5b15f223fc650ac31c6c5dcf1f89d71
Charlie-Dev-Des <charlie.ahlstrand01@gmail.com>
2023-04-07 16:21:24 +0200

    Restore

--- Assets/Code/AIMovement.cs
+++ Assets/Code/AIMovement.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class AIMovement : MonoBehaviour
 {
-
+    [SerializeField]
     private float speed;
-    
+    [SerializeField]
     private GameObject obj;
 
     AIMovement(float speed, GameObject obj) 
@@ -21,12 +21,12 @@ public class AIMovement : MonoBehaviour
         Vector3 vectorToTarget = new Vector3(obj.transform.position.x - transform.position.x, transform.position.y, obj.transform.position.z - transform.position.z);
 
         //Normalization of vector v / abs(v)
-        vectorToTarget = vectorToTarget / Mathf.Abs(vectorToTarget.x + vectorToTarget.y + vectorToTarget.z);
+        //vectorToTarget = vectorToTarget / Mathf.Abs(vectorToTarget.x + vectorToTarget.z);
 
         //Speed Adjustment
         vectorToTarget = new Vector3(vectorToTarget.x * speed, vectorToTarget.y, vectorToTarget.z * speed);
 
-        transform.position = vectorToTarget; 
+        transform.position = new Vector3(transform.position.x + vectorToTarget.x, transform.position.y, transform.position.z + vectorToTarget.z); 
     }
 
 }
