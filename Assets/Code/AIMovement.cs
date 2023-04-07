42d465a71a5c6d45f12c5ab78469fe1b86f4f9b8
Charlie-Dev-Des <charlie.ahlstrand01@gmail.com>
2023-04-07 15:54:28 +0200

    AIMovement and allocating scriptnames

---
+++ Assets/Code/AIMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMovement : MonoBehaviour
+{
+
+    private float speed;
+    
+    private GameObject obj;
+
+    AIMovement(float speed, GameObject obj) 
+    {
+        this.obj = obj;
+        this.speed = speed;
+    }
+
+    private void Update()
+    {
+        //Distance to target (x_2 - x_1), y, (z_2 - z_1)
+        Vector3 vectorToTarget = new Vector3(obj.transform.position.x - transform.position.x, transform.position.y, obj.transform.position.z - transform.position.z);
+
+        //Normalization of vector v / abs(v)
+        vectorToTarget = vectorToTarget / Mathf.Abs(vectorToTarget.x + vectorToTarget.y + vectorToTarget.z);
+
+        //Speed Adjustment
+        vectorToTarget = new Vector3(vectorToTarget.x * speed, vectorToTarget.y, vectorToTarget.z * speed);
+
+        transform.position = vectorToTarget; 
+    }
+
+}
