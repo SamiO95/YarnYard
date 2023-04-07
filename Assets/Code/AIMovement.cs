24605576c78f67d54e88dc2f5e608629017669d7
Charlie-Dev-Des <charlie.ahlstrand01@gmail.com>
2023-04-07 16:48:57 +0200

    AIMovement Done

--- Assets/Code/AIMovement.cs
+++ Assets/Code/AIMovement.cs
@@ -20,8 +20,9 @@ public class AIMovement : MonoBehaviour
         //Distance to target (x_2 - x_1), y, (z_2 - z_1)
         Vector3 vectorToTarget = new Vector3(obj.transform.position.x - transform.position.x, transform.position.y, obj.transform.position.z - transform.position.z);
 
-        //Normalization of vector v / abs(v)
-        //vectorToTarget = vectorToTarget / Mathf.Abs(vectorToTarget.x + vectorToTarget.z);
+        //Normalization of vector v.x / abs(v)
+        vectorToTarget.x = vectorToTarget.x / Mathf.Sqrt(vectorToTarget.x * vectorToTarget.x + vectorToTarget.z * vectorToTarget.z);
+        vectorToTarget.z = vectorToTarget.z / Mathf.Sqrt(vectorToTarget.x * vectorToTarget.x + vectorToTarget.z * vectorToTarget.z);
 
         //Speed Adjustment
         vectorToTarget = new Vector3(vectorToTarget.x * speed, vectorToTarget.y, vectorToTarget.z * speed);
