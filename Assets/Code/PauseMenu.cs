da0247bef95ebcfb85f4aa08ec4ac06f6d4039e4
Felicia Hjärpe <felicia.hjarpe@gmail.com>
2023-04-25 19:32:11 +0200

    UI update

--- Assets/Code/PauseMenu.cs
+++ Assets/Code/PauseMenu.cs
@@ -4,21 +4,30 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject pauseUI;
+
     // Start is called before the first frame update
     void Start()
     {
+        PauseScript p = GetComponent<PauseScript>();
 
+        if (p != null) 
+        {
+            p.PauseEvent += TogglePauseMenuUI;
+            p.ResumeEvent += TogglePauseMenuUI;
+        }
     }
 
     void TogglePauseMenuUI() 
     {
-        if (gameObject.active) 
-        { 
-            gameObject.SetActive(false);
+        if (pauseUI.active) 
+        {
+            pauseUI.SetActive(false);
         } 
         else 
         {
-            gameObject.SetActive(true);
+            pauseUI.SetActive(true);
         }
     }
 
