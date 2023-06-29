c4719a96ebce4c992a4a7388e1eb59fbd8f05fb0
SamiO95 <56935800+SamiO95@users.noreply.github.com>
2023-06-29 13:50:32 +0200

    Spawning
    The enemies spawn simultaneously, where you can decide the amount of enemies as well as the radius. But the rotation of the enemies are still wrong at the moment.

--- Assets/Code/EnemySpawner.cs
+++ Assets/Code/EnemySpawner.cs
@@ -1,25 +1,37 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public GameObject enemyPrefab; 
+    public GameObject enemyPrefab;
+    public int numEnemies = 5;
+    public float spawnRadius = 5f;
+    private bool hasSpawnedEnemies = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (!hasSpawnedEnemies && other.CompareTag("Player"))
         {
-            SpawnEnemy(other);
+            SpawnEnemies(other.gameObject);
+            hasSpawnedEnemies = true;
         }
     }
 
-    private void SpawnEnemy(Collider player)
+    private void SpawnEnemies(GameObject player)
     {
-       GameObject enemy =  Instantiate(enemyPrefab, transform.position, transform.rotation);
-       enemy.GetComponent<AIMovement>().SetObject(player.gameObject); 
+        float angleIncrement = 360f / numEnemies;
+        Vector3 playerPosition = player.transform.position;
+
+        for (int i = 0; i < numEnemies; i++)
+        {
+            float angle = i * angleIncrement;
+            Quaternion spawnRotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 spawnDirection = spawnRotation * Vector3.forward;
+            Vector3 spawnPosition = playerPosition + spawnDirection * spawnRadius;
+
+            GameObject enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+            enemy.transform.up = Vector3.up;
+            enemy.transform.rotation *= spawnRotation;
+            enemy.GetComponent<AIMovement>().SetObject(player);
+        }
     }
-
 }
-
