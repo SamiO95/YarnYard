263d04e6e2ff367587fdb3146055e9bbe18fe124
Charlie-Dev-Des <charlie.ahlstrand01@gmail.com>
2023-07-21 20:02:10 +0200

    player

--- Assets/Code/PlayerCharacter.cs
+++ Assets/Code/PlayerCharacter.cs
@@ -10,6 +10,7 @@ public class PlayerCharacter: AttackingCharacter
     private readonly int startingBaseDamage = 10;
     private readonly float startingReloadTime = 1f;
     private IWEAPON startingWeapon;
+    private List<EnemyCharacter> enemies;
 
     PlayerCharacter(int health, int baseDamage, float reloadTime, List<IWEAPON> startingWeapon) : base(health, baseDamage, reloadTime, startingWeapon)
     {}
@@ -17,6 +18,8 @@ public class PlayerCharacter: AttackingCharacter
     private void Start()
     {
         CreateNewPlayer();
+        enemies = new List<EnemyCharacter>();
+        Attack();
     }
 
     protected override void Die() 
@@ -34,5 +37,23 @@ public class PlayerCharacter: AttackingCharacter
         baseDamage = startingBaseDamage;
         reloadTime = startingReloadTime;
     }
+
+    public List<EnemyCharacter> GetEnemyList() 
+    {
+        return enemies;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Enemy"))
+        {
+            if (other.GetComponent<EnemyCharacter>() != null)
+            {
+                enemies.Add(other.GetComponent<EnemyCharacter>());
+                //Mark target fx?
+            }
+        }
+    }
+
 
 }
