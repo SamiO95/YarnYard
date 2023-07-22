dbf18ce413f33b9d80a395c796700147e7b577b4
Felicia Hjärpe <felicia.hjarpe@gmail.com>
2023-07-22 16:28:59 +0200

    MainMenu and Retry Scripts

--- Assets/Code/MainMenu.cs
+++ Assets/Code/MainMenu.cs
@@ -7,11 +7,21 @@ public class MainMenu : MonoBehaviour {
 
     public void PlayGame()
     {
+        //Loads first level after Main Menu
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadMainMenu() => SceneManager.LoadScene(0);
+
+    public void Retry()
+    {
+        //Restarts current level
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void QuitGame()
     {
+        //Exits the application
         Application.Quit();
     }
 
