79ff9f596155ed780eaec4dc7f74451849d8120b
Felicia Hjärpe <felicia.hjarpe@gmail.com>
2023-07-23 12:53:44 +0200

    Resolution and fullscreen and graphics fixed

--- Assets/Code/OptionsMenu.cs
+++ Assets/Code/OptionsMenu.cs
@@ -2,10 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer musicaudioMixer;
+
+    public TMPro.TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && 
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+
+        }
+    
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
     public void SetMusicVolume(float musicvolume)
     {
         musicaudioMixer.SetFloat("MusicVolume", musicvolume);
@@ -15,5 +53,15 @@ public class OptionsMenu : MonoBehaviour
     public void SetSFXVolume(float sfxvolume)
     {
         sfxaudioMixer.SetFloat("SFXVolume", sfxvolume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
